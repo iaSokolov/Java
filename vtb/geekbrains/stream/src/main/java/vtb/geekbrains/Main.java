8e8284f467b01b5a107927d990cb0ad948ac036f
iaSokolov <iasokolov@gmail.com>
2021-01-09 16:18:12 +0300

    stream

--- vtb/geekbrains/stream/src/main/java/vtb/geekbrains/Main.java
+++ vtb/geekbrains/stream/src/main/java/vtb/geekbrains/Main.java
@@ -1,16 +1,18 @@
 package vtb.geekbrains;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
-    Stream<WordCounter> intStream =
-            Stream.of("1","2","3")
-            .map(s -> new WordCounter(s, 1))
-            .
-
-
+    public static void main(String[] args) {
+        //получить наиболее часто встречающееся слово в массиве
+        String word = Arrays.asList("Майя", "Марк", "Инна", "Майя", "Инна", "Инна")
+                .stream()
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max((o1, o2) -> o1.getValue() - o2.getValue() > 0 ? 1 : -1).get().getKey();
+        System.out.println(word);
+    }
 }
 
 
