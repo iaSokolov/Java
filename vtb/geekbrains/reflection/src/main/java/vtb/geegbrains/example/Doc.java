24ec7d65ab83df390d8ac99c3e8248df3d0c0818
iaSokolov <iasokolov@gmail.com>
2021-01-13 19:58:34 +0300

    reflection

---
+++ vtb/geekbrains/reflection/src/main/java/vtb/geegbrains/example/Doc.java
@@ -0,0 +1,52 @@
+package vtb.geegbrains.example;
+
+//Задачи:
+// Создать валидатор на основе аннотаций:
+//@Max(int length) - максимальное значение (все Number)
+//@Min(int length) - минимальное значение (все Number)
+//@Regexp(String regexp) - подходит ли по регулярному выражению (только String)
+//@NotNull - применяется для всех ссылочных типов
+//@NotEmpty - применяется для всех наследников Collections
+//      Вместо интерфейса необходимо создать класс, который с помощью Reflection API будет делать валидацию по созданным аннотациям:
+//class Validator {
+//  boolean validate(Object obj);
+//}
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public interface Doc {
+
+    @Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE,ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Max {
+        int length() default Integer.MAX_VALUE; // все Number
+    }
+
+    @Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Min {
+        int length() default Integer.MIN_VALUE; // все Number
+
+    }
+
+    @Target({ElementType.FIELD, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Regexp { //применяется для строк, подходит ли по регулярному выражению
+        String reg() default "\\D+";
+    }
+
+    //применяется для всех ссылочных типов
+    @Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface NotNull {
+    }
+
+    //@NotEmpty - применяется для всех наследников Collections
+    @Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface NotEmpty {
+    }
+}
---
+++ vtb/geekbrains/reflection/src/main/java/vtb/geekbrains/App.java
@@ -0,0 +1,7 @@
+package vtb.geekbrains;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
