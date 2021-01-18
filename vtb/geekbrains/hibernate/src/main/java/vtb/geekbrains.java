49fbc7efb5c2a1ac0c9ef1b078874c2ac98f36d3
iaSokolov <iasokolov@gmail.com>
2021-01-18 15:48:48 +0300

    подключение к postresql через jdbc

--- vtb/geekbrains/hibernate/src/main/java/vtb/geekbrains.java
+++ vtb/geekbrains/hibernate/src/main/java/vtb/geekbrains.java
@@ -2,11 +2,27 @@ package vtb;
 
 import de.vandermeer.asciitable.AsciiTable;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class geekbrains {
     public static void main(String[] args) {
-        AsciiTable at = new AsciiTable();
-        at.addRule();
-        at.addRow("Column 1", "Column 2");
-        System.out.println(at.render());
+//        AsciiTable at = new AsciiTable();
+//        at.addRule();
+//        at.addRow("Column 1", "Column 2");
+//        System.out.println(at.render());
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = null;
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/single","postgres", "admin");
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
