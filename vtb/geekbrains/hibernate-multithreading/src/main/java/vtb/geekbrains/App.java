208973f6efdc90c7492a83e608a075e6e1c225c1
iaSokolov <iasokolov@gmail.com>
2021-02-21 14:14:43 +0300

    hibernate-miltithreading init DB

---
+++ vtb/geekbrains/hibernate-multithreading/src/main/java/vtb/geekbrains/App.java
@@ -0,0 +1,32 @@
+package vtb.geekbrains;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class App {
+    public static SessionFactory factory;
+    public static void main(String[] args) {
+        try {
+            factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .buildSessionFactory();
+
+            factory.getCurrentSession().beginTransaction();
+
+
+
+            String request = Files.lines(Paths.get("factory.sql")).collect(Collectors.joining(" "));
+            factory.getCurrentSession().createNativeQuery(request).executeUpdate();
+            factory.getCurrentSession().getTransaction().commit();
+        } catch (Exception ex) {
+            System.out.println("ошибка: " + ex.getMessage());
+        } finally {
+            factory.getCurrentSession().close();
+            factory.close();
+        }
+    }
+}
