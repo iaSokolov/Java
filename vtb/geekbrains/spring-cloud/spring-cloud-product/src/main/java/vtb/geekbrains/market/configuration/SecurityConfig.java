e7f0c13f394d9fbb6a574765f0ffd74ac3976246
Ilya Sokolov <iasokolov@gmail.com>
2021-04-05 17:19:27 +0300

    spring-cloud init jwt авторизация

---
+++ vtb/geekbrains/spring-cloud/spring-cloud-product/src/main/java/vtb/geekbrains/market/configuration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package vtb.geekbrains.market.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity(debug = false)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                ;//.and()
+                //.formLogin().loginPage("/login")//.defaultSuccessUrl("/")
+                //.and()
+                //.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
+        http.exceptionHandling().accessDeniedPage("/403");
+    }
+}
